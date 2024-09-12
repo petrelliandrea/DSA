d77aaab84c9982999971b27ca73d86cd2fa3622c
Andrea Debian <petrelliandrea200@gmail.com>
2024-09-12 21:30:24 +0200

    Edit queueArray.js

--- sort/queueArray.js
+++ sort/queueArray.js
@@ -1,3 +1,5 @@
+// Type FIFO (First In First Outr)
+
 class Queue {
   constructor() {
     this.elements = [];
@@ -5,25 +7,25 @@ class Queue {
     this.tail = 0;
   }
 
-  enqueue(element) {
+  enqueue(element) { // insert on the queue
     this.elements[this.tail++] = element;
   }
 
-  dequeue() {
+  dequeue() {   // remove element 
     const removedElement = this.elements[this.head++];
     delete this.elements[this.head - 1];
     return removedElement;
   }
 
-  peek() {
+  peek() { // return num of element
     return this.elements[this.head];
   }
 
-  isEmpty() {
+  isEmpty() { // return element is empty 
     return this.tail - this.head === 0;
   }
 
-  size() {
+  size() { // return size of queue
     return this.tail - this.head;
   }
 }
@@ -31,6 +33,11 @@ class Queue {
 
 const f = new Queue;
 f.enqueue(10);
+f.enqueue(20);
+f.enqueue(30);
 
-
-console.log(f.size())
+console.log("Size: " +f.size());
+console.log("Primo elemento: " + f.peek());
+f.dequeue();
+console.log("Size :" + f.size());
+console.log("Primo elemento: " + f.peek());
